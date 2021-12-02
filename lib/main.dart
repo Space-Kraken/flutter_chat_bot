6e7b6669077915f5b08bf4306c85899e0e691d74
MrDonut <ridik_ecp@hotmail.com>
2021-12-02 14:27:18 -0600

    Completed Flutter ChatBot with DialogFlow

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,32 @@
+import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        brightness: Brightness.dark,
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -48,68 +35,168 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  final msgController = TextEditingController();
+  List<Map> msgs = [];
+  void response(query) async {
+    AuthGoogle authGoogle = await AuthGoogle(fileJson: "assets/service.json").build();
+    Dialogflow dialogflow = Dialogflow(authGoogle: authGoogle, language: Language.english);
+    AIResponse response = await dialogflow.detectIntent(query);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      msgs.insert(0, {
+        // "data": DateFormat('kk:mm').format(DateTime.now()),
+        'data': 0,
+        'message': response.getListMessage()[0]['text']['text'][0].toString(),
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Flutter Chatbot"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Center(
+              child: Container(
+                padding: EdgeInsets.only(top: 15, bottom: 10),
+                child: Text("Today, ${DateFormat("Hm").format(DateTime.now())}", style: TextStyle(fontSize: 20),),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+
+            Flexible(
+              child: ListView.builder(
+                reverse: true,
+                itemCount: msgs.length,
+                itemBuilder: (context, index) => chat(
+                  msgs[index]['message'].toString(), 
+                  msgs[index]['data'],  
+                ),
+              )
             ),
+
+            const Divider(
+              height: 5,
+              color: Colors.greenAccent,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Container(
+                child: ListTile(
+                  title: Container(
+                    height: 40,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      color: Color.fromRGBO(220, 220, 220, 1)
+                    ),
+                    padding: const EdgeInsets.only(left: 15),
+                    child: TextFormField(
+                      controller: msgController,
+                      decoration: const InputDecoration(
+                        hintText: "Type your message here",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none
+                      ),
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black
+                      ),
+                    ),
+                  ),
+                  trailing: IconButton( 
+                    icon: const Icon(Icons.send),
+                    iconSize: 30,
+                    color: Colors.greenAccent,
+                    onPressed: (){
+                      if(msgController.text.isEmpty){
+                        print("Empty");
+                      }else{
+                        setState(() {  
+                          msgs.insert(0, {
+                            "data":1,
+                            "message": msgController.text,
+                          });
+                        });
+                        response(msgController.text);
+                        msgController.clear();
+                      }
+                      FocusScopeNode currentFocus = FocusScope.of(context);
+                      if(!currentFocus.hasPrimaryFocus){
+                        currentFocus.unfocus();
+                      }
+                    },
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+  Widget chat(String msg, int data){
+    return Container(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Row(
+        mainAxisAlignment: data == 1 ? MainAxisAlignment.end: MainAxisAlignment.start,
+        children: [
+          data == 0 ? Container(
+            height: 60,
+            width: 60,
+            child: CircleAvatar(
+              backgroundImage: AssetImage("assets/bot.png"),
+            ),
+          ): Container(),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Bubble(
+              radius: const Radius.circular(15),
+              color: data == 0 ? const Color.fromRGBO(23, 157, 139, 1): Colors.orangeAccent,
+              elevation: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(2),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Flexible(
+                      child: Container(
+                        constraints: const BoxConstraints(maxWidth: 200),
+                        child: Text(
+                          msg,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),
+                        )
+                      )
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          data == 1 ? Container(
+            height: 60,
+            width: 60,
+            child: CircleAvatar(
+              backgroundImage: AssetImage("assets/user.png"),
+            ),
+          ): Container(), 
+        ],
+      ),
     );
   }
 }
+
